904e7656a3c9bac6fc16512d949369e4923dcf49
akashkumar491 <visitak7@gmail.com>
2022-10-06 10:24:42 +0530

    modified base price frontend

--- src/components/BasePriceTable.tsx
+++ src/components/BasePriceTable.tsx
@@ -1,43 +1,50 @@
 import { useEffect, useState } from "react";
-import axios from 'axios'
-import './style.css'
+import axios from "axios";
+import "./style.css";
 import { Link } from "react-router-dom";
+
 const BasePriceTable = () => {
-  const rows: { id: number, amount: number, created_at: string, updated_at: string }[] = [];
+  const rows: {
+    id: number;
+    amount: number;
+    created_at: string;
+    updated_at: string;
+  }[] = [];
 
   const [tableData, setTableData] = useState(rows);
-  const [basePriceForm, setBasePriceForm] = useState(0);
+  const [basePriceForm, setBasePriceForm] = useState({
+    amount: 0,
+    fromTime: "",
+    toTime: "",
+  });
   const [isUpdateFlag, setIsUpdateFlag] = useState(false);
+  const [updateItemId, setUpdateItemId] = useState(-1);
 
-  const getBasePriceTable = async () => {
-    fetch("http://localhost:8080/basePrice")
+  const getBasePriceTable = () => {
+    fetch("http://localhost:7000/slotsPrice?date=2022-08-09")
       .then((res: any) => res.json())
-      .then((res: any) => setTableData(res.message))
-      .catch((err: any) => console.log(err))
+      .then((res: any) => setTableData(res.basePrice))
+      .catch((err: any) => console.log(err));
   };
 
   const handleSubmit = async (e: any) => {
-    e.preventDefault()
+    e.preventDefault();
     if (!isUpdateFlag) {
-      if (tableData.length > 0) {
-        window.alert("Cannot Add More Then 1 Table")
-        setBasePriceForm(0);
-      } else {
-        try {
-          await axios.post(`http://localhost:8080/basePrice`, { amount: basePriceForm })
-          setBasePriceForm(0);
-          getBasePriceTable()
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    } else {
       try {
-        console.log(basePriceForm)
-        await axios.patch(`http://localhost:8080/basePrice/${tableData[0].id}`, { amout: basePriceForm })
-        getBasePriceTable()
-        setBasePriceForm(0);
-        setIsUpdateFlag(false)
+        await axios.post(`http://localhost:7000/basePrice`, basePriceForm);
+        setBasePriceForm({ amount: 0, fromTime: "", toTime: "" });
+        getBasePriceTable();
+      } catch (error) {
+        console.log(error);
+      }
+
+    } else {
+      
+      try {
+        await axios.put(`http://localhost:7000/editBasePrice/${updateItemId}`, basePriceForm);
+        getBasePriceTable();
+        setBasePriceForm({ amount: 0, fromTime: "", toTime: "" });
+        setIsUpdateFlag(false);
       } catch (error) {
         console.log(error);
       }
@@ -47,34 +54,68 @@ const BasePriceTable = () => {
   useEffect(() => {
     getBasePriceTable();
   }, []);
-
-
+  
+  
   const handleUpdate = async (item: any) => {
-    setBasePriceForm(item.amount);
-    setIsUpdateFlag(true)
+    if(item.fromTime && item.toTime) {
+      setBasePriceForm((prev)=>({ ...prev, amount: item.amount,
+        fromTime: new Date(item.fromTime).toUTCString().split(" ")[4],
+        toTime: new Date(item.toTime).toUTCString().split(" ")[4] 
+      }));
+
+    } else {
+
+      setBasePriceForm((prev)=>({ ...prev, amount: item.amount, fromTime:"", toTime: "" }));
+    }
+
+    setIsUpdateFlag(true);
+    setUpdateItemId(item.id);
   };
+
   const handleDelete = async (item: any) => {
     try {
-      await axios.delete(`http://localhost:8080/basePrice/${item.id}`)
-      let array = tableData
-      array = array.filter((itemObj: any) => itemObj.id !== item.id)
-      setTableData(array)
-      setIsUpdateFlag(false)
+      await axios.delete(`http://localhost:7000/basePrice/${item.id}`);
+      let array = tableData;
+      array = array.filter((itemObj: any) => itemObj.id !== item.id);
+      setTableData(array);
+      setIsUpdateFlag(false);
     } catch (error) {
       console.log(error);
     }
-
   };
+
   const handleChangeHandler = (e: any) => {
-    setBasePriceForm(e.target.value)
-  }
+    setBasePriceForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
   return (
     <div className="basePriceContainer">
       <h1 className="h1header basePrice">Base Price</h1>
       <form className="form-basePrice" onSubmit={handleSubmit}>
-        <label>Enter Amount</label>
-        <input type="number" value={basePriceForm} onChange={(e) => handleChangeHandler(e)} />
-        <button className="submitbtn" type="submit">{isUpdateFlag ? ("SAVE") : ("SUBMIT")}</button>
+        <label>Amount</label>
+        <input
+          type="number"
+          value={basePriceForm.amount}
+          name="amount"
+          onChange={(e) => handleChangeHandler(e)}
+        />
+        <label>From Time</label>
+        <input
+          type="time"
+          value={basePriceForm.fromTime}
+          name="fromTime"
+          onChange={(e) => handleChangeHandler(e)}
+        />
+        <label>To Time</label>
+        <input
+          type="time"
+          value={basePriceForm.toTime}
+          name="toTime"
+          onChange={(e) => handleChangeHandler(e)}
+        />
+        <button className="submitbtn" type="submit">
+          {isUpdateFlag ? "SAVE" : "SUBMIT"}
+        </button>
       </form>
       <div>
         <table className="tbl styled-table">
@@ -82,6 +123,8 @@ const BasePriceTable = () => {
             <tr>
               <th>ID</th>
               <th>AMOUNT</th>
+              <th>FROM TIME</th>
+              <th>END TIME</th>
               <th>CREATED AT</th>
               <th>UPDATED AT</th>
               <th>ACTIONS</th>
@@ -94,8 +137,16 @@ const BasePriceTable = () => {
                   <tr key={item.id}>
                     <td>{item.id}</td>
                     <td>{item.amount}</td>
-                    <th>{item.created_at.split("T")[0]} {item.created_at.split("T")[1].split(".000Z")[0]}</th>
-                    <th>{item.updated_at.split("T")[0]} {item.updated_at.split("T")[1].split(".000Z")[0]}</th>
+                    <td>
+                      {item.fromTime &&
+                        new Date(item.fromTime).toUTCString().split(" ")[4]}
+                    </td>
+                    <td>
+                      {item.toTime &&
+                        new Date(item.toTime).toUTCString().split(" ")[4]}
+                    </td>
+                    <th>{new Date(item.create_at).toLocaleString()}</th>
+                    <th>{new Date(item.update_at).toLocaleString()}</th>
                     <td>
                       <button
                         className="btn updt"
@@ -117,7 +168,9 @@ const BasePriceTable = () => {
           </tbody>
         </table>
       </div>
-      <Link to='/slotPrice' className="slot" >Slot Price</Link>
+      <Link to="/slotPrice" className="slot">
+        Slot Price
+      </Link>
     </div>
   );
 };
